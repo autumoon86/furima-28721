e37b2ec7bea557dc60236c19ba820f8ed7465b18
autumoon86 <eepsc86@gmail.com>
2020-09-15 18:46:36 +0900

    エラーメッセージの表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-         
+  validates :first_name, presence: true
+  validates :family_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :family_name_kana, presence: true
+  validates :birthday, presence: true     
 end
