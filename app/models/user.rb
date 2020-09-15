522bc46d15ead81e4729b7d3ef2ac367669775d5
autumoon86 <eepsc86@gmail.com>
2020-09-15 20:33:33 +0900

    with_optionの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :family_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :family_name_kana, presence: true
-  validates :birthday, presence: true
-  
+  with_options presence: true do
+   validates :nickname
+   validates :first_name
+   validates :family_name
+   validates :first_name_kana
+   validates :family_name_kana
+   validates :birthday
+  end
+
   VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
   validates :password, format: { with: VALID_PASSWORD_REGEX }
 
