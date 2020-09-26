3405e3a25104fbff5693fec1b9f62087a7e3874f
autumoon86 <eepsc86@gmail.com>
2020-09-26 17:15:54 +0900

    ログインの有無で遷移先変更、直接URLを指定した際の遷移先変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,11 @@
 class OrdersController < ApplicationController
   before_action :set_item, only: [:index, :create]
-
+  before_action :authenticate_user!
 
   def index
+    if current_user.id == @item.user_id || @item.order
+      redirect_to root_path
+    end
     @address_order = AddressOrder.new
   end
 
